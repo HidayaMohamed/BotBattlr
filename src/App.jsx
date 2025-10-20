a9530ac76cdbf2fd8d26838563b6d3f71bc68faf
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-10-20 05:05:11 +0300

    Adds Functions that handle enlistment, releasing snd discharging

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,39 @@
 import { useState } from 'react';
 import React from 'react'
 import BotCollection from './BotCollection'
+import BotArmy from './BotArmy';
 
 function App() {
-
+  const [bots, setBots] = useState([]);
   const [armyBots, setArmyBots] = useState([]);
 
   function handleEnlistment(bot) {
-    if (!armyBots.find(b => b.id === bot.id)) {
-       setArmyBots([ ...armyBots, bot])
-     }  // const [bots, setBots] = useState([]);
-   }
+    if (!armyBots.find((b) => b.id === bot.id)) {
+      setArmyBots([...armyBots, bot]);
+    }
+  }
+
+  function handleRelease(bot) {
+  setArmyBots(armyBots.filter(b => b.id !== bot.id));
+  }
+
+  function handleDischarge() {
+    fetch("http://localhost:3000/bots", {
+      method: "DELETE",
+    }).then(res => res.json())
+      .then(() => {
+      setBots(bots.filter(b => b.id === bot.id))
+      })
+    .catch(err => console.error("DElete failed", err))
+  }
+
 
   return (
     <div>
-      <BotCollection onEnlist={handleEnlistment } />
+      <BotCollection bots={bots} onEnlist={handleEnlistment} />
+      <BotArmy armyBots={armyBots} onRelease={handleRelease}  onDischarge={handleDischarge} />
     </div>
-  )
+  );
 }
 
 export default App
