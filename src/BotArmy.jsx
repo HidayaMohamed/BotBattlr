0c384c8a7e434a12f4387319a3917dab234f18f1
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-10-20 10:54:49 +0300

    Adds comment to BotArmy.jsx

--- src/BotArmy.jsx
+++ src/BotArmy.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+// Imports BotCard.
 import BotCard from './BotCard'
-
+// Takes in armyBots, onRelease and onDischarge as props(destructured)
 function BotArmy({armyBots, onRelease, onDischarge}) {
   return (
     <div className="bot-army">
       <h2>Your Bot Army</h2>
-      <div>
+          <div>
+              {/* Displays the army in an unordered list */}
         <ul className="army-list">
           {armyBots.map((bot) => (
             <li key={bot.id} className="army-item">
