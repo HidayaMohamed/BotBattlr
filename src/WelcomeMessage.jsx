6c3e94ca5307d18e3240753083482a00dbf8ec57
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-10-20 10:34:50 +0300

    Adds a WElcomeMessage function

---
+++ src/WelcomeMessage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function WelcomeMessage() {
+  return (
+    <div>
+      <h1>Welcome to Bot Battlr</h1>
+      <p>
+        The one and only spot in the known universe where you can custom build
+        your own Bot Army!
+      </p>
+    </div>
+  );
+}
+
+export default WelcomeMessage
