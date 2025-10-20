a00a4d24623f0bc4fe7bae5f69bfe9ec45f7be97
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-10-20 04:47:20 +0300

    ADds a botcard

---
+++ src/BotCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function BotCard({bot, onClick, onDischarge }) {
+  return (
+    <div className="bot-card" onClick={() => onClick(bot)}>
+      <h2>{bot.name}</h2>
+      <img src="{bot.avatar_url}" alt="{bot.name}" />
+      <p>
+        Health - {bot.health}
+        <br></br>
+        Damage - {bot.damage} <br></br>
+        Armor - {bot.armor} <br></br>
+        Class - {bot.bot_class} <br></br>
+        CatchPharse - {bot.catchphrase} <br></br>
+        created - {bot.created_at} <br></br>
+        Updated at - {bot.updated_at}
+          </p>
+          <button onClick={(e) => {
+              e.stopPropagation()
+              onDischarge(bot)
+          }
+          } > x Discharge
+          </button>
+    </div>
+  );
+}
+
+export default BotCard
