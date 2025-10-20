f04d5ace369b61f903bd953a53b4dc1a7ede2d3b
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-10-20 10:59:47 +0300

    Adds comments to BotCard.jsx

--- src/BotCard.jsx
+++ src/BotCard.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+// Imports BotSpecs 
 import BotSpecs from './BotSpecs';
-
+// Takes in destructured props
 function BotCard({bot, onClick, onDischarge }) {
   return (
     <div className="bot-card">
       <h2>{bot.name}</h2>
       <img src={bot.avatar_url} alt={bot.name} />
-      <BotSpecs bot={bot}/>
-      <button onClick={() => onClick(bot)}>Release</button>
+          <BotSpecs bot={bot} />
+          {/* Clicking this button releases the bot */}
+          <button onClick={() => onClick(bot)}>Release</button>
+          {/* Clickimg this button deletes the bot */}
       <button
         onClick={(e) => {
           e.stopPropagation();
@@ -15,7 +18,7 @@ function BotCard({bot, onClick, onDischarge }) {
         }}
         style={{ backgroundColor: "red" }}
       >
-        x Discharge
+        x 
       </button>
     </div>
   );
